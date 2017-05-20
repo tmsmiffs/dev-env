d0e5f8cdeedf05f6edec203aed701d2deacd8b3c
Marshall Smith <tmsmith@gmail.com>
2017-05-20 10:28:45 -0700

    Enable linting with eslint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,8 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
+
 const PORT = 3000;
 let app = express();
 let compiler = webpack(config);
