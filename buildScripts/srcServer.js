d2978c98046669e27185c23a5e7c7be36fcd6ab4
Marshall Smith <tmsmith@gmail.com>
2017-05-19 23:23:12 -0700

    Add babel for transpiling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
-var port = 3000;
-var app = express();
+const PORT = 3000;
+let app = express();
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
+app.listen(PORT, function(err) {
     if (err) {
         console.log(err);
     } else {
-        open('http://localhost:' + port);
+        open('http://localhost:' + PORT);
     }
 });
